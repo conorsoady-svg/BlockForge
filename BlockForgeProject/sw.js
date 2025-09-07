fd7dc129e30502891a88f93978d933bcf31176d2
Conor Soady <conorsoady@gmail.com>
2025-09-08 00:53:44 +0100

    Initial commit

---
+++ BlockForgeProject/sw.js
@@ -0,0 +1,43 @@
+// sw.js
+const CACHE = 'bf-v1';
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE).then(cache =>
+      cache.addAll([
+        './',
+        './BlockForge.html',
+        // add icons/manifest if you have them:
+        // './manifest.webmanifest',
+        // './icons/icon - 192x192.png',
+        // './icons/icon - 512x512.png',
+      ]).catch(err => {
+        console.warn('Cache addAll warning:', err);
+      })
+    )
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(event.request).then(cached =>
+      cached ||
+      fetch(event.request).then(resp => {
+        const copy = resp.clone();
+        caches.open(CACHE).then(c => c.put(event.request, copy));
+        return resp;
+      }).catch(() => cached)
+    )
+  );
+});
