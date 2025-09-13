4754f2e279674bf6388b134c229542b9b139ae7d
Conor Soady <conorsoady@gmail.com>
2025-09-13 20:08:02 +0100

    fix: switch to file-based service worker and clean HTML registration

--- sw.js
+++ sw.js
@@ -1,43 +1,29 @@
 // sw.js
 const CACHE = 'bf-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/192x192.png',
+  './icons/512x512.png'
+];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
   self.skipWaiting();
-  event.waitUntil(
-    caches.open(CACHE).then(cache =>
-      cache.addAll([
-        './',
-        './BlockForge.html',
-        // add icons/manifest if you have them:
-        // './manifest.webmanifest',
-        // './icons/192x192.png',
-        // './icons/512x512.png',
-      ]).catch(err => {
-        console.warn('Cache addAll warning:', err);
-      })
-    )
-  );
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
+    )
   );
+  self.clients.claim();
 });
 
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-
-  event.respondWith(
-    caches.match(event.request).then(cached =>
-      cached ||
-      fetch(event.request).then(resp => {
-        const copy = resp.clone();
-        caches.open(CACHE).then(c => c.put(event.request, copy));
-        return resp;
-      }).catch(() => cached)
-    )
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
