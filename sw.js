d9b20fec8430def732ce4f1bb3990036b28e4799
Conor Soady <conorsoady@gmail.com>
2025-09-22 19:34:09 +0100

    18

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* sw.js — update-prompt style */
-const CACHE = "bf-static-v7";
+const CACHE = "bf-static-v8";
 
 /* Optional: very small precache (safe files only) */
 self.addEventListener("install", (event) => {
