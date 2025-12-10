a30ff3c6878b6ece31e17ed8b7aa9e3c2c2bcf0f
Conor Soady <conorsoady@gmail.com>
2025-12-10 00:25:09 +0000

    again agressivley

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* sw.js — update-prompt style */
-const CACHE = "bf-static-v59";
+const CACHE = "bf-static-v60";
 
 /* Optional: very small precache (safe files only) */
 self.addEventListener("install", (event) => {
